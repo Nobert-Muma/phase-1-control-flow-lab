bd3db141eb7a4c62c16c54066f61bd9bab12c857
Nobert-Muma <mumanobert17@gmail.com>
2024-06-24 22:45:55 +0300

    Control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+    if (someValue <= 400) {
+      return "This one is on me!";
+    } else if (someValue >= 400 && someValue <= 2000) {
+      return "That will be twenty bucks.";
+    } else if (someValue > 2500) { 
+      return "No can do.";
+    } else { 
+      return `I will gladly take your thirty bucks.`; 
+    }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const result = (city === "NYC") ? "Ok, sounds good." : "No go.";
+  return result;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(charmFromTip){
   // Write your code here!
+  switch(charmFromTip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+      case "not as generous":
+        return "Thank you.";
+        break;
+        case "thanks for everything":
+          return "Bye."
+          break;
+  }
 }
